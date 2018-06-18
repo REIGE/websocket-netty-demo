62a4c740894485f84a58e858a73f4f720a8194d6
reige <407601459@qq.com>
2018-06-18 17:57:53 +0800

    简单websocket + netty demo

---
+++ src/main/java/com/reige/websocket/Main.java
@@ -0,0 +1,33 @@
+package com.reige.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoop;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author chenrui
+ * @date 2018/6/18.
+ */
+public class Main {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workGroup);
+            bootstrap.channel(NioServerSocketChannel.class);
+            bootstrap.childHandler(new MyWebSocketChannelHandler());
+            System.out.println("服务端开启等待客户端连接...");
+            Channel channel = bootstrap.bind(8888).sync().channel();
+            channel.closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/reige/websocket/MyWebSocketChannelHandler.java
@@ -0,0 +1,24 @@
+package com.reige.websocket;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+
+/**
+ * @author chenrui
+ * @date 2018/6/18.
+ */
+public class MyWebSocketChannelHandler extends ChannelInitializer<SocketChannel>{
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ch.pipeline().addLast("http-codec", new HttpServerCodec());
+        ch.pipeline().addLast("aggregator", new HttpObjectAggregator(65536));
+        ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+        ch.pipeline().addLast("handler", new MyWebSocketHandler());
+    }
+}
